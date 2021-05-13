79beef4256e202e3fc8e42a81173ad191228491f
uniloftsky <uniloftsky@gmail.com>
2021-05-13 12:16:30 +0300

    minor fixes with an exception handling

--- src/main/java/com/uniloftsky/springframework/taskmanager/controllers/GlobalController.java
+++ src/main/java/com/uniloftsky/springframework/taskmanager/controllers/GlobalController.java
@@ -4,16 +4,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.zip.DataFormatException;
+import java.time.format.DateTimeParseException;
 
 @ControllerAdvice
 public class GlobalController {
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(DataFormatException.class)
+    //    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DateTimeParseException.class)
     public String handleInvalidDate(Exception exception, Model model) {
+        System.out.println("dsadsa");
         model.addAttribute("error", exception);
-        return "error";
+        return "datetimeparseerror";
     }
 
 }
